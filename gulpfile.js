070f1ac453f8b9c8c25eefa009ebcb80149be93b
ase <anton.sementsov@unymira.com>
2019-04-02 21:43:53 +0200

    working sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
+var sass = require('gulp-sass');
 require('dotenv').config();
 
 var user = process.env.USER;
@@ -15,7 +16,7 @@ var localFiles = [
 
 var remoteLocation = './www/';
 
-function getFtpConnection(){
+async function getFtpConnection(){
     return ftp.create({
         host: "anton-sementsov.bplaced.net",
         port: 21,
@@ -32,4 +33,17 @@ gulp.task('deploy',function(){
     return gulp.src(localFiles, {base: '.', buffer: false})
         .pipe(conn.newer(remoteLocation))
         .pipe(conn.dest(remoteLocation))
+});
+
+//compile sass
+gulp.task('sass', async function () {
+    gulp.src('src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+    });
+
+//default    
+gulp.task('default', function(done) {  //<---- Insert 'done' as a parameter here...
+gulp.series('sass')
+done(); //<---- ...and call it here
 })
